3a2110c384a1ca859a9fa25151bef495ff947918
Mat B <91279878+D3jag0re@users.noreply.github.com>
2025-01-02 10:04:35 -0500

    varS

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "do_spaces_access_key" {
+  description = "The access key for DigitalOcean Spaces."
+  type        = string
+  sensitive   = true
+}
+
+variable "do_spaces_secret_key" {
+  description = "The secret key for DigitalOcean Spaces."
+  type        = string
+  sensitive   = true
+}
+
+
+variable "aws_access_key_id" {
+  description = "The secret key for AWS."
+  type        = string
+  sensitive   = true
+}
+
+
+variable "aws_secret_access_key" {
+  description = "The secret key for AWS."
+  type        = string
+  sensitive   = true
+}
